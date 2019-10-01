25ef9ee623c5f28251e2a9def64009c8b355143c
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-10-01 20:53:10 +0300

    Update AirLineTest.cs

--- TestFlightProject/AirLineTest.cs
+++ TestFlightProject/AirLineTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using FlightsManagementSystem;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,28 +8,62 @@ namespace TestFlightProject
     [TestClass]
     public class AirLineTest
     {
-        LoggedInAirlineFacade AirLine = new LoggedInAirlineFacade();        
+        LoggedInAirlineFacade facade = FlyingCenterSystem.GetInstance().GetFacade("LLL", "111", out ILoginToken token) as LoggedInAirlineFacade;
         LoginToken<AirlineCompany> AirLineLogin = new LoginToken<AirlineCompany>();
-        FacadeBase facade = FlyingCenterSystem.GetInstance().GetFacade("LLL", "111", out ILoginToken token);
         Flight flight = new Flight(10079,13,14,new DateTime (2019, 08, 09, 10, 00, 00),new DateTime (2019, 08, 09, 12, 00, 00), 30);
         [TestMethod]
         public void Login()
         {
-            Assert.AreEqual(facade.GetType(), AirLine.GetType());
+            Assert.AreEqual(facade.GetType(), facade.GetType());
         }
 
         [TestMethod]
         public void CreateFlight()
         {
-            IList<AirlineCompany> airlines = AirLine.GetAllAirlineCompanies();
+            IList<AirlineCompany> airlines = facade.GetAllAirlineCompanies();
             flight.AirlineCompanyId = airlines[0].Id;
-            AirLine.CreateFlight(AirLineLogin, flight);
+            facade.CreateFlight(AirLineLogin, flight);
         }
         [TestMethod]
         public void GetAllFlights()
         {           
-            AirLine.GetAllFlights(AirLineLogin);
+            facade.GetAllFlights(AirLineLogin);
         }
-        
+        [TestMethod]
+        public void UpdateFlight()
+        {
+            IList<AirlineCompany> airlines = facade.GetAllAirlineCompanies();
+            flight.AirlineCompanyId = airlines[0].Id;
+            flight.LandingTime = new DateTime(2019, 11, 11, 10, 00, 00);
+            facade.UpdateFlight(AirLineLogin, flight);
+        }
+        [TestMethod]
+        public void ModifyAirlineDetails()
+        {
+            IList<AirlineCompany> airlines = facade.GetAllAirlineCompanies();
+            airlines[0].UserName = "LLL";
+            facade.ModifyAirlineDetails(AirLineLogin, airlines[0]);
+        }
+        [TestMethod]
+        public void CancelFlight()
+        {
+            IList<AirlineCompany> airlines = facade.GetAllAirlineCompanies();
+            flight.AirlineCompanyId = airlines[0].Id;
+            facade.CancelFlight(AirLineLogin, flight);
+        }
+        [TestMethod]
+        public void GetAllTickets()
+        {
+            IList<Ticket> tickets = facade.GetAllTickets(AirLineLogin);
+        }
+        [TestMethod]
+        public void ChangeMyPassword()
+        {
+            LoginToken<AirlineCompany> AirLineLogin = new LoginToken<AirlineCompany>
+            {
+                User = facade.GetAllAirlineCompanies()[0]
+            };
+            facade.ChangeMyPassword(AirLineLogin, "111", "222");            
+        }
     }
 }
