79538c090273481ddfa440ac76fbfdceff1f4ad8
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-10-01 20:54:07 +0300

    Add files via upload

---
+++ TestFlightProject/CustomerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using FlightsManagementSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestFlightProject
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        LoggedInCustomerFacade facade = FlyingCenterSystem.GetInstance().GetFacade("YR","1234", out ILoginToken token) as LoggedInCustomerFacade;
+        LoginToken<Customer> CustomerLogin = new LoginToken<Customer>();
+        
+        [TestMethod]
+        public void Login()
+        {
+            Assert.AreEqual(facade.GetType(), facade.GetType());
+        }
+        [TestMethod]
+        public void CancelTicket()
+        {
+            Ticket ticket = new Ticket(facade.GetAllFlights()[0].Id, 10113);
+            facade.CancelTicket(CustomerLogin, ticket);
+        }
+        [TestMethod]
+        public void GetAllMyFlights()
+        {            
+            facade.GetAllMyFlights(CustomerLogin);
+        }
+        [TestMethod]
+        public void PurchaseTicket()
+        {
+            AdministratorTest AT = new AdministratorTest();
+            LoginToken<Administrator> AdminLogin = new LoginToken<Administrator>();
+            Ticket ticket = new Ticket(facade.GetAllFlights()[0].Id, AT.GetCustomerByUserName("YR"));
+            facade.PurchaseTicket(CustomerLogin,ticket);
+            
+        }
+
+    }
+}
